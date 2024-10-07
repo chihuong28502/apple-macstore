684c180bedd83f02256f1e3f2c669cb4810ced8d
chihuong28502 <chihuong28502@gmail.com>
2024-10-07 18:04:20 +0700

    build cors

--- health-backend/src/main.ts
+++ health-backend/src/main.ts
@@ -8,10 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  const corsOrigins = configService.get<string>('CORS_ORIGIN', '').split(',');
   // const reflector = app.get(Reflector);
   // app.useGlobalGuards(new BlockApiGuard(reflector));
   app.enableCors({
-    origin: 'https://health-sand-alpha.vercel.app',
+    origin: corsOrigins.length > 1 ? corsOrigins : corsOrigins[0],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
