622a5e4c924a0e4aa80ba79dcf4384020dbbd61a
chihuong28502 <chihuong28502@gmail.com>
2024-10-21 15:02:32 +0700

    log

--- health-frontend/src/middleware.ts
+++ health-frontend/src/middleware.ts
@@ -3,6 +3,7 @@ import createMiddleware from "next-intl/middleware";
 import { NextRequest, NextResponse } from "next/server";
 
 const basePaths = ["private", "product"];
+console.log("🚀 ~ basePaths:", basePaths)
 const privatePaths = locales.flatMap((locale) =>
   basePaths.map((path) => `/${locale}/${path}`)
 );
@@ -20,7 +21,9 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const accessToken = request.cookies.get("accessToken")?.value;
+  console.log("🚀 ~ accessToken:", accessToken)
   const refreshToken = request.cookies.get("refreshToken")?.value;
+  console.log("🚀 ~ refreshToken:", refreshToken)
   const role = request.cookies.get("role")?.value; // Kiểm tra role từ cookie
   const locale = request.cookies.get("NEXT_LOCALE")?.value ?? defaultLocale;
 
