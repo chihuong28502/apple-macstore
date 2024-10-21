c25139b763938684a80357e3684e851df4fe8e96
chihuong28502 <chihuong28502@gmail.com>
2024-10-21 14:59:03 +0700

    fix cookies

--- health-frontend/src/hooks/Cookies.ts
+++ health-frontend/src/hooks/Cookies.ts
@@ -1,5 +1,13 @@
-import Cookies from 'js-cookie';
-
 export function getCookie(name: string): string | undefined {
-  return Cookies.get(name);
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+
+  // Kiểm tra nếu mảng có 2 phần tử
+  if (parts.length === 2) {
+    const lastPart = parts.pop(); // Lấy phần tử cuối cùng
+    if (lastPart) { // Kiểm tra nếu phần tử không phải undefined
+      return lastPart.split(';').shift(); // Trả về phần tử đầu tiên sau khi chia
+    }
+  }
+  return undefined; // Trả về undefined nếu không tìm thấy cookie
 }
