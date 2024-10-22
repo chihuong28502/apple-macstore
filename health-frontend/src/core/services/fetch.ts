ec7e9f12f7e5d5e3efc3509d3a61749c329c2845
chihuong28502 <chihuong28502@gmail.com>
2024-10-22 20:16:57 +0700

    fix api

--- health-frontend/src/core/services/fetch.ts
+++ health-frontend/src/core/services/fetch.ts
@@ -9,7 +9,7 @@ import { store } from "./store";
 import { AuthUtils } from "@/lib/localAuth";
 
 export let AxiosClient = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL_LOCAL || '/api',
   timeout: CONST.REQUEST.REQUEST_TIMEOUT,
   headers: {
     "Content-Type": "application/json",
