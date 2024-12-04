4d73a51a150b214abcac22f80d8f04a84e0599c0
chihuong28502 <chihuong28502@gmail.com>
2024-12-04 20:08:52 +0700

    fix close sidebar

--- frontend/src/layouts/LayoutHome.tsx
+++ frontend/src/layouts/LayoutHome.tsx
@@ -19,7 +19,6 @@ const LayoutHome = ({ children }: { children: ReactNode }) => {
     <div className="overflow-hidden flex flex-col">
       <Header />
       <div className="flex flex-1 overflow-hidden mt-12 pt-1">
-        {/* <SideBar collapsed={false} /> */}
         <main className="flex-1 overflow-auto px-4 flex flex-col justify-between bg-mainContent">
           <div className="flex-grow">{children}</div>
           <div className="">
