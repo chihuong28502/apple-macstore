dccb439676325e10f2723b810fe7298f0d4c6550
Chí Hưởng <bigbang02xs@gmail.com>
2024-12-06 15:32:58 +0000

    Update providers.tsx

--- frontend/src/providers/providers.tsx
+++ frontend/src/providers/providers.tsx
@@ -1,22 +1,19 @@
-import ReduxProvider from "@/core/components/ReduxProvider";
-import LayoutHome from "@/layouts/LayoutHome";
-import 'antd/dist/reset.css';
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
+
+import ReduxProvider from "@/core/components/ReduxProvider";
+import LayoutHome from "@/layouts/LayoutHome";
+
 import AuthProvider from "./AuthProvider";
 import { RootStyleRegistry } from "./RootStyleRegistry";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 export async function Providers({ children }: { children: ReactNode }) {
   return (
     <ReduxProvider>
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <RootStyleRegistry>
-
-          <AuthProvider>
-            <LayoutHome>{children}</LayoutHome>
-          </AuthProvider>
+            <AuthProvider>
+              <LayoutHome>{children}</LayoutHome>
+            </AuthProvider>
         </RootStyleRegistry>
       </ThemeProvider>
     </ReduxProvider>
